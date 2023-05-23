e362985e5682732f011efa2aa957676033e2dd74
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-05-23 22:54:30 +0530

    Program to check whther a number is even or odd
    Program to check whther a number is even or odd by using predicate

---
+++ Predefine Functional Interface/MyPredicate.java
@@ -0,0 +1,15 @@
+package may_23_2023;
+
+//Program to check whther a number is even or odd
+
+import java.util.function.*;
+import java.util.Scanner;
+public class MyPredicate {
+	public static void main(String [] args) {
+			Predicate<Integer> p =  x -> x%2==0;
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter a Number :");
+			int num = sc.nextInt();
+			System.out.println(num +" is even ?"+p.test(num));
+	}
+}
