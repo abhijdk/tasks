d801963fb2dc11d86f3b3e355e4f51a5218939ca
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-05-24 19:20:28 +0530

    converts a number to a string
    //"Write a Java program that converts a number to a string using a predefined functional interface."

---
+++ Predefine Functional Interface/My_Pre_Fun2.java
@@ -0,0 +1,16 @@
+package may_24_2023;
+
+//"Write a Java program that converts a number to a string using a predefined functional interface."
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class My_Pre_Fun2 {
+	 public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter your Name");
+	        int number = sc.nextInt();
+	        
+	        Function<Integer, String> f=x-> String.valueOf(number);
+	        System.out.println(f.apply(number));
+	    }
+}
