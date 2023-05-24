7f5125e422cec1b636675ac7bdac182d63b235eb
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-05-24 20:59:50 +0530

    return the string using Supplier
    "Write a Java program that return the string using Supplier functional interface."

---
+++ Predefine Functional Interface/My_Pre_Fun4.java
@@ -0,0 +1,17 @@
+package may_24_2023;
+
+//"Write a Java program that find the length of a string using Consumer functional interface."
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class My_Pre_Fun4 {
+	 public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter your Name");
+	        String s=sc.nextLine();
+	        
+	        Consumer<String> con=x->System.out.println(x.length());
+	        con.accept(s);
+	    }
+}
